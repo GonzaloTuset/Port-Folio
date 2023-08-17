92e898fa206c6fc914690836a6cf073076bb3d96
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-08-17 18:14:23 -0300

    Intengracion scroll Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import Header from "./components/Header"
 function App() {
-
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section instanceof HTMLElement) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <main className={`bg-white w-screen h-screen `}>
-      <div className="bg-black text-white">HOLA</div>
+      <Header scrollToSection={scrollToSection}/>
+      <section id="home" className="bg-cyan-500">
+        <div className="text-white">HOLA</div>
+      </section>
+      <section id="about" className="bg-black text-white h-[100px] mt-[1000px]">
+        <div>Fuck</div>
+      </section>
+      
       </main>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
-const Header = () => {
- return(
-  <div></div>
- )
+interface HeaderProps {
+  scrollToSection: (sectionId: string) => void;
 }
+const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
+  return (
+    <div>
+      <a onClick={() => scrollToSection("home")}>Home</a>
+      <a onClick={() => scrollToSection("about")}>About Me</a>
+    </div>
+  );
+};
 
 export default Header
