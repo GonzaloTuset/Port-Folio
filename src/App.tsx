92234c717a55feb0ba891b205a975f74ca1148a6
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-08-23 18:26:22 -0300

    pequeñas adiciones

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,12 @@ function App() {
     <Header scrollToSection={scrollToSection}/>
     <main className={`bg-white w-screen h-screen `}>
       <section id="home" className="bg-cyan-500 ">
-        <div className="text-black">HOLA</div>
+        <article className="col-12 col-lg-6 text-white px-3 px-lg-5 ">
+          <h2 className="text-black">Gonzalo Tuset</h2>
+          <p>React Developer</p>
+          <a>Descargar Cv</a>
+          <div className="bg-black">Mi nombre es Gonzalo, tengo 23 años,recido en Buenos Aires,Argentina.</div>
+        </article>
          </section>
       <section id="about" className="bg-black text-white h-[100px] mt-[1000px]">
         <div>Fuck</div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,10 @@ interface HeaderProps {
 }
 const Header: React.FC<HeaderProps> = ({ scrollToSection }) => {
   return (
-    <div className="fixed top-0 left-0 w-full bg-white p-4 shadow z-50">
+    <div className="fixed top-0 left-0 w-full bg-[rgb(17,17,17)] p-4 shadow z-50 text-[rgb(234,249,249)] ">
       <a onClick={() => scrollToSection("home")}>Home</a>
       <a onClick={() => scrollToSection("about")}>About Me</a>
+      <img></img>
     </div>
   );
 };
