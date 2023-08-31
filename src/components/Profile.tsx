e3bf62fa08cc2578408d6bba680a367540f5b336
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-08-31 19:08:00 -0300

    mas estilos

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,9 @@
 import perfil from '../assets/Perfil.jpg'
 const Profile: React.FC = () => {
   return(
-    <div className="bg-black w-[374px] h-[350px]">
-      <div><span className='text-white text-2xl/[54px]'>Gonzalo Tuset</span></div>
-      <div className='flex flex-row'><bdi className='text-white'>Full Stack Web Developer</bdi><div className='flex items-center'><img src="https://community.cloudflare.steamstatic.com/public/images/countryflags/ar.gif"/></div><p className='text-white'>Argentina</p></div>
+    <div className="bg-black w-[374px] h-[350px] pl-[8px] pr-[8px] pt-[8px]">
+      <div><span className='text-white text-2xl text-ellipsis'>Gonzalo Tuset</span></div>
+      <div className='flex flex-row '><bdi className='text-white mr-[10px]'>Full Stack Web Developer</bdi><div className='flex items-center'><img src="https://community.cloudflare.steamstatic.com/public/images/countryflags/ar.gif"/></div><p className='text-white'>Argentina</p></div>
       <div className='w-[123px] border-cyan-300 border-2'>
         <img src={perfil}></img>
       </div>
