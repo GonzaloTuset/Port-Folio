336f01facd55a2fc71298c3d2ea5cb8250c6bc4b
GonzaloTuset <110003160+GonzaloTuset@users.noreply.github.com>
2023-09-04 19:25:26 -0300

    mas cambios

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,7 +1,13 @@
 const Info: React.FC = () => {
   return(
-  <div className=" w-[375px] h-[2000px] card">
-    hola
+  <div className=" w-[375px] h-[300px] card px-[3px] flex flex-col">
+    <div className='w-[367.5px] h-[45px] info_trasn flex  items-center mb-[12px]'>
+      <a className="family text-[20px] mx-[12px] my-[12px] text-[rgba(81,188,206)]">Open To Work</a>
+    </div>
+    <div className="h-[200px] w-[367.5px]">
+      <div className=" bg-[rgba(0,0,0,0.5)]"></div>
+      <div className=" bg-[rgba(0,0,0,0.5)]"></div>
+    </div>
   </div>
   )
 }
